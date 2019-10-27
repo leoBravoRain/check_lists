78dd34cca08f3b5b5831d56bca605e6142f8bb6c
leo <leo.bravo.rain@gmail.com>
2019-10-27 00:52:43 -0300

    add question in header in downloaded files from web app

--- web/check_lists_web/src/screens/lists_by_category/lists_by_category.screen.js
+++ web/check_lists_web/src/screens/lists_by_category/lists_by_category.screen.js
@@ -116,118 +116,142 @@ class Lists_by_Category extends React.Component {
     download_responses(id_list, list_name) {
 
         // get request for get data
-        // fs.collection(this.props.match.params.category + "_responses").where("id_list", "==", id_list).get()
-        fs.collection("answers").where("id_list", "==", id_list).get()
+        // at first get questions of list
+        fs.collection("list_new_structure").doc(id_list).get()
+        
+            .then(doc => {
 
-            .then(snapshotquery => {
+                if (doc.exists) {
 
-                console.log(snapshotquery);
-                // // get data from API
-                
-                // if query is not empty
-                if (!snapshotquery.empty) {
-                    
-                    var lists = [];
+                    // console.log("Document data:", doc.data());
+                    const questions_text = doc.data().questions;
 
-                    // iterate over each item
-                    snapshotquery.forEach(doc => {
-                        
-                        // add loteo to list
-                        lists.push(doc.data().answers);
-                        // console.log(doc.data().answers);
-                        
-                    });
-
-                    // console.log(lists);
-
-                    // try to create csv file and download
-                    try {
-
-                        console.log("starting try");
-
-                        // empty workbook object
-                        var wb = XLSX.utils.book_new();
-
-                        console.log(wb);
-
-                        wb.Props = {
-                            Title: list_name,
-                            // Subject: "Test",
-                            // Author: "Red Stapler",
-                            // CreatedDate: new Date(2017, 12, 19)
-                        };
-
-                        // add sheet
-                        wb.SheetNames.push("Respuestas");
-
-                        // add content to sheet
-                        // var ws_data = [['quiero', 'un', 'dron']];  //a row with 2 columns
-                        var ws_data = lists;
-
-                        console.log("ws data: ", ws_data);
-
-                        // create the sheet from this array
-                        var ws = XLSX.utils.aoa_to_sheet(ws_data);
-
-                        console.log("ws: ", ws);
-
-                        // assign sheet to workbook
-                        wb.Sheets["Respuestas"] = ws;
-
-                        console.log("befatore wbout");
-
-                        // export workbook as xlsx binary
-                        var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
-
-                        console.log("wbout: ", wbout);
-
-                        function s2ab(s) {
-                            var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
-                            var view = new Uint8Array(buf);  //create uint8array as viewer
-                            for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
-                            return buf;
-                        }
-
-                        // $("#button-a").click(function () {
-                        saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), list_name + '.xlsx');
-
-                        // console.log(wb);
-
-                        // this work for .csv and it can open with libreoffice but not with excel
-                        // // parser data to csv format
-                        // const parser = new Parser();
-                        // const csv = parser.parse(lists);
-
-                        // // create csv file
-                        // let csvContent = "data:text/csv;charset=utf-8," + csv;
-                        // var encodedUri = encodeURI(csvContent);
-                        // // create hidden link
-                        // var link = document.createElement("a");
-                        // link.setAttribute("href", encodedUri);
-                        // link.setAttribute("download", list_name + ".csv");
-                        // document.body.appendChild(link); // Required for FF
-                        // // This will download the data file named "my_data.csv".
-                        // link.click();
-
-                    } 
-                    // if there is some error
-                    catch (err) {
-                        window.alert("Hemos tenido un problema al crear y descargar el archivo con las respuestas, inténtalo de nuevo por favor");
-                    }
-                }
-                // if query is empty
-                else {
-                    window.alert("Aún no existen respuestas para esta lista");
+                    // get answers of list
+                    fs.collection("answers").where("id_list", "==", id_list).get()
+            
+                        .then(snapshotquery => {
+            
+                            console.log(snapshotquery);
+                            // // get data from API
+                            
+                            // if query is not empty
+                            if (!snapshotquery.empty) {
+                                
+                                var answers = [];
+                                // add questions as header
+                                answers.push(questions_text);
+                                
+                                // iterate over each item
+                                snapshotquery.forEach(doc => {
+                                    
+                                    // add loteo to list
+                                    answers.push(doc.data().answers);
+                                    // console.log(doc.data().answers);
+                                    
+                                });
+            
+                                // console.log(answers);
+            
+                                // try to create csv file and download
+                                try {
+            
+                                    console.log("starting try");
+            
+                                    // empty workbook object
+                                    var wb = XLSX.utils.book_new();
+            
+                                    console.log(wb);
+            
+                                    wb.Props = {
+                                        Title: list_name,
+                                        // Subject: "Test",
+                                        // Author: "Red Stapler",
+                                        // CreatedDate: new Date(2017, 12, 19)
+                                    };
+            
+                                    // add sheet
+                                    wb.SheetNames.push("Respuestas");
+            
+                                    // add content to sheet
+                                    // var ws_data = [['quiero', 'un', 'dron']];  //a row with 2 columns
+                                    var ws_data = answers;
+            
+                                    console.log("ws data: ", ws_data);
+            
+                                    // create the sheet from this array
+                                    var ws = XLSX.utils.aoa_to_sheet(ws_data);
+            
+                                    console.log("ws: ", ws);
+            
+                                    // assign sheet to workbook
+                                    wb.Sheets["Respuestas"] = ws;
+            
+                                    console.log("befatore wbout");
+            
+                                    // export workbook as xlsx binary
+                                    var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+            
+                                    console.log("wbout: ", wbout);
+            
+                                    function s2ab(s) {
+                                        var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+                                        var view = new Uint8Array(buf);  //create uint8array as viewer
+                                        for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+                                        return buf;
+                                    }
+            
+                                    // $("#button-a").click(function () {
+                                    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), list_name + '.xlsx');
+            
+                                    // console.log(wb);
+            
+                                    // this work for .csv and it can open with libreoffice but not with excel
+                                    // // parser data to csv format
+                                    // const parser = new Parser();
+                                    // const csv = parser.parse(answers);
+            
+                                    // // create csv file
+                                    // let csvContent = "data:text/csv;charset=utf-8," + csv;
+                                    // var encodedUri = encodeURI(csvContent);
+                                    // // create hidden link
+                                    // var link = document.createElement("a");
+                                    // link.setAttribute("href", encodedUri);
+                                    // link.setAttribute("download", list_name + ".csv");
+                                    // document.body.appendChild(link); // Required for FF
+                                    // // This will download the data file named "my_data.csv".
+                                    // link.click();
+            
+                                } 
+                                // if there is some error
+                                catch (err) {
+                                    window.alert("Hemos tenido un problema al crear y descargar el archivo con las respuestas, inténtalo de nuevo por favor");
+                                }
+                            }
+                            // if query is empty
+                            else {
+                                window.alert("Aún no existen respuestas para esta lista");
+                            }
+                        })
+            
+            
+                        // if error
+                        .catch(function (error) {
+            
+                            console.log("error!");
+                            // dislpay error in console
+                            console.log(error);
+            
+                        });
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
                 }
             })
-
-
-            // if error
             .catch(function (error) {
 
-                console.log("error!");
+                console.log("error: " + error);
                 // dislpay error in console
-                console.log(error);
+                // console.log(error);
 
             });
 
